d9ff8d9db5a71bb36d47b76884bd74b01639467c
LV11coding <94009217+LV11coding@users.noreply.github.com>
2024-08-16 21:06:47 +0300

    Add files via upload

---
+++ Peg_Soliraire.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int tx[33];
+    int ix[33];
+    fill_n(tx, 33, 0);
+    fill_n(ix, 33, 0);
+    tx[0] = 3;
+    ix[0] = 3;
+    int en = 0;
+    int var = 1;
+
+    while (en < 32)
+    {
+        cout << "Enter vertical: ";
+        cin >> tx[var];
+        cout << "Enter horizontal: ";
+        cin >> ix[var];
+
+        for (int i = 0; i < 7; i++)
+        {
+            for (int t = 0; t < 7; t++)
+            {
+                if (t < 2 && i < 2)
+                {
+                    cout << ' ';
+                }
+                else if (t > 4 && i < 2)
+                {
+                    cout << ' ';
+                }
+                else if (i > 4 && t < 2)
+                {
+                    cout << ' ';
+                }
+                else if (i > 4 && t > 4)
+                {
+                    cout << ' ';
+                }
+                else if (i == 3 && t == 3)
+                {
+                    cout << ' ';
+                }
+                else
+                {
+                    bool match = false;
+                    bool check1 = false;
+                    for (int z = 0; z <= var; z++)
+                    {
+                        if (i == ix[z] && t == tx[z])
+                        {
+                            match = true;
+                            break;
+                        }
+                        else if (i == ix[var] && t == tx[var])
+                        {
+                            check1 = true;
+                        }
+                    }
+                    if (check1)
+                    {
+                        for (int f = 0; f < 33; f++)
+                        {
+                            if (tx[var] == tx[f] && ix[var] - 2 == ix[f])
+                            {
+                                cout << ' ';
+                                break;
+                            }
+                            else if (tx[var] + 2 == tx[f] && ix[var] == ix[f])
+                            {
+                                cout << ' ';
+                                break;
+                            }
+                            else if (tx[var] == tx[f] && ix[var] + 2 == ix[var])
+                            {
+                                cout << ' ';
+                                break;
+                            }
+                            else if (tx[var] - 2 == tx[f] && ix[var] == ix[f])
+                            {
+                                cout << ' ';
+                                break;
+                            }
+                            else if (f > 31)
+                            {
+                                cout << 'x';
+                                break;
+                            }
+                        }
+                    }
+                    else if (match)
+                    {
+                        cout << ' ';
+                    }
+                    else
+                    {
+                        cout << 'x';
+                    }
+                }
+            }
+            cout << endl;
+        }
+        var++;
+        en++;
+    }
+    return 0;
+}
